767432abc3853bd2603cf09db060dcce7e91fe43
Sambasivarao <samba.pendela@gmail.com>
2022-02-01 08:25:13 +0530

    code

--- spa-demo/src/Master.js
+++ spa-demo/src/Master.js
@@ -6,13 +6,15 @@ import { BrowserRouter as Router, Route, NavLink,Routes } from "react-router-dom
 function Master(){
     return(
        <Router>
-           <NavLink to="/page_one" activeStyle={{color:"green"}} exact={true} strict style={{marginRight:100}}>Page_One</NavLink>
-           <NavLink to="/page_two" activeStyle={{color:"green"}} exact={true} strict style={{marginRight:100}}>Page_Two</NavLink>
-           <NavLink to="/page_three" activeStyle={{color:"green"}} exact={true} strict style={{marginRight:100}}>Page_Three</NavLink>
-           <br></br><br></br>
-           <Route to="/page_one" component={Pageone} exact={true} strict></Route>
-           <Route to="/page_two" component={Pagetwo} exact={true} strict></Route>
-           <Route to="/page_three" component={Pagethree} exact={true} strict></Route>
+             <NavLink to="/">Pageone</NavLink>
+              <NavLink to="/page_two">Pagetwo</NavLink>
+              <NavLink to="/page_three">Pagethree</NavLink>
+          <Routes>
+            
+              <Route path="" element={Pageone}></Route>
+              <Route path="page_two" element={Pagetwo}></Route>
+              <Route path="page_three" element={Pagethree}></Route>
+          </Routes>
        </Router>
     )
 }
