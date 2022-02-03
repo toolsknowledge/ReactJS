8045b7b5e191e1648e8ef337a6199faaf40c6b10
Sambasivarao <samba.pendela@gmail.com>
2022-02-03 10:08:13 +0530

    miniproject

---
+++ miniproject_server/server.js
@@ -0,0 +1,59 @@
+//import the modules
+//require() function used to import the modules
+const express = require("express");
+const mongodb = require("mongodb");
+const cors = require("cors");
+const jwt = require("jwt-simple");
+
+
+
+//create the rest object
+let app = express();
+
+
+//enable the cors policy
+app.use(cors());
+
+
+//set the json as MIME Type
+app.use(express.json());
+
+
+//create the client object
+let ashokIT = mongodb.MongoClient;
+//where "ashokIT" is the client object
+
+
+let server_token = "";
+
+
+//create the post request
+app.post("/login",(req,res)=>{
+    ashokIT.connect("mongodb+srv://admin:admin@03reactjs9am.7kkvt.mongodb.net/miniproject?retryWrites=true&w=majority",(err,connection)=>{
+        if(err) throw err;
+        else{
+            let db = connection.db("miniproject");
+            db.collection("login_details").find({"email":req.body.email,"password":req.body.password}).toArray((err,array)=>{
+                if(err) throw err;
+                else{
+                    if(array.length>0){
+                        //token
+                        //converting readable data to unreadable data with custom password called as token
+                        let token = jwt.encode({"email":req.body.email,"password":req.body.password},"admin123");
+                        server_token = token;
+                        res.send({"login":"success","token":token});
+                    }else{
+                        res.send({"login":"fail"});
+                    }
+                }
+            });
+        }
+    });
+});
+
+
+
+//assign the port number
+app.listen(8080,()=>{
+    console.log("server listening the port no.8080");
+});
